3fee58265557ad03df9de93fa9f017199f4622c4
ktm_kiddo <mateograif@gmail.com>
2022-05-10 07:30:01 +0545

    Major changes

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+
+const AboutPage = () => {
+    <main>
+        <div>
+        <title>About!</title>
+        <h1>We at Mateo-Ink want to provide the kitties of nepal with food, shelter and snuggles</h1>
+        <p>Help us with our project!</p>
+        </div>
+    </main>
+}
+
+    export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import * as React from "react"
 const IndexPage = () => {
  return (
    <main>
+     <div>
       <title>"My site!"</title>
       <h1>Welcome to my site!</h1>
       <p>For da kitties!!!!!!</p>
+      </div>
    </main>
+   
  ) 
 
 }
